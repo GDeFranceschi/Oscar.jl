482d0db907d049899e9a121ec0f347ac11c0f31e
Giovanni De Franceschi <giovanni.defranceschi@auckland.ac.nz>
2020-11-16 16:48:57 +0100

    Start unipotent centralizer

--- src/Groups/classical_groups/linear_centralizer.jl
+++ src/Groups/classical_groups/linear_centralizer.jl
@@ -10,8 +10,43 @@ function _elem_given_det(x,d)
    return prod([C[i]^Int(l[i]) for i in 1:ngens(C)])
 end
 
-# given an irreducible polynomial f in F[t], return the matrix that generates the field { g(C(f)) | g in F[t] }
-# where C(f) = companion_matrix(f)
-function _gen_matrix
+# returns as matrices
+# TODO: uses gen(F) for a generator of the multiplicative group of F
+function _gens_for_GL(n::Int, F::Ring)
+   if n==1 return matrix(F,1,1,[gen(F)]) end
+   if order(F)==2
+      h1 = identity_matrix(F,n)
+      h1[1,2] = 1
+      h2 = zero_matrix(F,n,n)
+      for i in 1:n-1 h2[i+1,i]=1 end
+      h2[1,n] = 1
+   else
+      h1 = identity_matrix(F,n)
+      h1[1,1] = gen(F)
+      h2 = zero_matrix(F,n,n)
+      for i in 1:n-1 h2[i+1,i]=-1 end
+      h2[1,1] = -1
+      h2[1,n] = 1
+   end
+   return h1,h2      
 end
 
+# V = vector of integers of the dimensions of Jordan blocks
+# return the generators for the centralizers of the unipotent element
+# assumes V is sorted (e.g. [1,1,1,2,3,3])
+function _centr_unipotent(F::Ring, V::AbstractVector{Int}) 
+   
+   # L = multiset(V)
+   L=[[V[1],1]]
+   for i in 2:length(V)
+      if V[i]==L[length(L)][1]
+         L[length(L)][2] += 1
+      else
+         L = cat(L, [[V[i],1]]; dims=1)
+      end
+   end
+   listgens = MatElem[]
+   
+   
+
+end
