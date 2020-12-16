3213475455cf1466b6944fc52a32e6fd49897b93
Giovanni De Franceschi <giovanni.defranceschi@auckland.ac.nz>
2020-12-16 16:46:16 +0100

    Forms

--- src/Groups/classical_groups/stuff_isometries.jl
+++ src/Groups/classical_groups/stuff_isometries.jl
@@ -1,15 +1,25 @@
-export preserved_quadratic_forms, preserved_sesquilinear_forms
+import AbstractAlgebra: FieldElem
+import Hecke: gram_matrix
+
+export 
+    alternating_form,
+    hermitian_form,
+    preserved_quadratic_forms,
+    preserved_sesquilinear_forms,
+    SesquilinearForm,
+    symmetric_form
 
 mutable struct SesquilinearForm{T<:RingElem}
    matrix::MatElem{T}
-   descr::Symbol       # quadratic, bilinear or sesquilinear
-   pol::PolyElem{T}     # only for quadratic forms
+   descr::Symbol       # quadratic, bilinear or hermitian
+   pol::AbstractAlgebra.Generic.MPoly{T}     # only for quadratic forms
    X::GapObj
 
+   SesquilinearForm(B::MatElem{T},sym) where T = new{T}(B,sym)
 end
 
 
-GAP.Globals.LoadPackage(GAP.julia_to_gap("Forms"))
+GAP.Packages.load("forms")
 
 """
     preserved_quadratic_forms(G::MatrixGroup)
@@ -20,7 +30,7 @@ Return a generating set for the vector space of quadratic forms preserved by `G`
 
 """
 function preserved_quadratic_forms(G::MatrixGroup)
-   L = GAP.Globals.PreservedQuadraticrForms(G.X)
+   L = GAP.Globals.PreservedQuadraticForms(G.X)
    return [G.mat_iso(GAP.Globals.GramMatrix(L[i])) for i in 1:length(L)]
 end
 
@@ -36,3 +46,91 @@ function preserved_sesquilinear_forms(G::MatrixGroup)
    L = GAP.Globals.PreservedSesquilinearForms(G.X)
    return [G.mat_iso(GAP.Globals.GramMatrix(L[i])) for i in 1:length(L)]
 end
+
+"""
+    alternating_form(B::MatElem{T}; check=true)
+Return the alternating form with Gram matrix `B`. If `check` is set as `false`, it does not check whether the matrix is skew-symmetric.
+"""
+function alternating_form(B::MatElem{T}; check=true) where T <: FieldElem
+   if check
+      for i in 1:nrows(B)
+      for j in i:nrows(B)
+         @assert B[i,j]==-B[j,i] "The matrix is not skew-symmetric"
+      end
+      end
+      if characteristic(base_ring(B))==2
+         for i in 1:nrows(B)
+            @assert B[i,i]==0 "The matrix is not skew-symmetric"
+         end
+      end
+   end
+   f = SesquilinearForm(B, :alternating)
+   return f
+end
+
+"""
+    symmetric_form(B::MatElem{T}; check=true)
+Return the symmetric form with Gram matrix `B`. If `check` is set as `false`, it does not check whether the matrix is symmetric.
+"""
+function symmetric_form(B::MatElem{T}; check=true) where T <: FieldElem
+   if check
+      for i in 1:nrows(B)
+      for j in i+1:nrows(B)
+         @assert B[i,j]==B[j,i] "The matrix is not symmetric"
+      end
+      end
+   end
+   f = SesquilinearForm(B, :symmetric)
+   return f
+end
+
+"""
+    hermitian_form(B::MatElem{T}; check=true)
+Return the hermitian form with Gram matrix `B`. If `check` is set as `false`, it does not check whether the matrix is hermitian.
+"""
+function hermitian_form(B::MatElem{T}; check=true) where T <: FieldElem
+   if check
+      @assert iseven(degree(base_ring(B))) "The matrix is not hermitian"
+      e = div(degree(base_ring(B)),2)
+      for i in 1:nrows(B)
+      for j in i:nrows(B)
+         @assert B[i,j]==frobenius(B[j,i],e) "The matrix is not hermitian"
+      end
+      end
+   end
+   f = SesquilinearForm(B, :hermitian)
+   return f
+end
+
+
+########################################################################
+#
+# Show
+#
+########################################################################
+
+
+function _assign_description(sym::Symbol)
+   if sym== :alternating print("Alternating")
+   elseif sym== :symmetric print("Symmetric")
+   else error("unsupported description")
+   end
+end
+
+
+function Base.show(io::IO, B::SesquilinearForm)
+   _assign_description(B.descr)
+   println(" form with Gram matrix ")
+   show(io, B.matrix)
+end
+
+
+
+########################################################################
+#
+# Fields of the variable
+#
+########################################################################
+
+gram_matrix(B::SesquilinearForm) = B.matrix
+
--- src/Groups/classical_groups/vector_spaces.jl
+++ src/Groups/classical_groups/vector_spaces.jl
@@ -1,11 +1,16 @@
+import AbstractAlgebra: FieldElem
 
 Base.getindex(V::AbstractAlgebra.Generic.FreeModule, i::Int) = gens(G, i)
 
 # this just allows to write V([1,z,0]) instead of V([F(1),z,F(0)])
-(V::AbstractAlgebra.Generic.FreeModule)(l::Array{Any,1}) = V( [base_ring(V)(j) for j in l] )
+function (V::AbstractAlgebra.Generic.FreeModule)(l::Array{T,1}) where T
+   if T<:typeof(base_ring(V)) return V(l)
+   else return V( [base_ring(V)(j) for j in l] )
+   end
+end
 
 # scalar product
-Base.:*(v::AbstractAlgebra.Generic.FreeModuleElem{T},u::AbstractAlgebra.Generic.FreeModuleElem{T}) where T <: (v.v*transpose(u.v))[1]
+Base.:*(v::AbstractAlgebra.Generic.FreeModuleElem{T},u::AbstractAlgebra.Generic.FreeModuleElem{T}) where T <: FieldElem = (v.v*transpose(u.v))[1]
 
 Base.:*(v::AbstractAlgebra.Generic.FreeModuleElem{T},x::MatElem{T}) where T <: FieldElem = v.parent(v.v*x)
 Base.:*(x::MatElem{T},u::AbstractAlgebra.Generic.FreeModuleElem{T}) where T <: FieldElem = x*transpose(u.v)
