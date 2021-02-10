b282ef1eee9757fc7af2440272deee842616c257
Giovanni De Franceschi <giovanni.defranceschi@auckland.ac.nz>
2021-02-10 15:17:27 +0100

    Further from PR #173

--- src/Groups/GAPGroups.jl
+++ src/Groups/GAPGroups.jl
@@ -109,7 +109,7 @@ function order(x::Union{GAPGroupElem, GAPGroup})
    return GAP.gap_to_julia(GAP.Globals.Order(x.X))
 end
 
-function order(::Type{T}, x::Union{GAPGroupElem, GAPGroup}) where T<:Number
+function order(::Type{T}, x::Union{GAPGroupElem, GAPGroup}) where T<: Number
    return GAP.gap_to_julia(T, GAP.Globals.Order(x.X))
 end
 
